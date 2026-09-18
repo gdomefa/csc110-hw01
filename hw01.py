07c2030392fcdcec3051c2816224978be42c7b62
gdomefa <gdomefa@smith.edu>
2026-09-18 11:16:47 -0400

    Solved part 4

--- hw01.py
+++ hw01.py
@@ -94,7 +94,8 @@ def main():
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
-
+    result4 = 100 % 13
+    print("Part 4: result =", result4)
     # End of Part 4 ----------------------
 
 if __name__ == "__main__":
